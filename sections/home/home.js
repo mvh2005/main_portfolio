638f4080774ab9b7f66382212cc82f17d4748a6f
Mara Vignesh <vigneshmara143@gmail.com>
2026-06-13 11:35:22 +0530

    Update home.js

--- sections/home/home.js
+++ sections/home/home.js
@@ -24,7 +24,7 @@ customElements.define('portfolio-home', class extends HTMLElement {
               <a href="#projects" class="btn btn-primary">View My Work</a>
               <a href="sections/resume-modal/resume_vignesh.pdf" class="btn btn-ghost" target="_blank" rel="noopener noreferrer">Resume</a>
               <a href="https://github.com/mvh2005" class="btn btn-ghost" target="_blank" rel="noopener noreferrer">GitHub</a>
-              <a href="https://www.linkedin.com/in/vignesh-mara-561942279/" class="btn btn-ghost" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+              <a href="https://www.linkedin.com/in/mara-vignesh-561942279/" class="btn btn-ghost" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </div>
           </div>
         </div>
