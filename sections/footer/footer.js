03c0a87b8dfae915c378901ef3ee7464a0547c9b
Mara Vignesh <vigneshmara143@gmail.com>
2026-06-13 11:34:43 +0530

    Update footer.js

--- sections/footer/footer.js
+++ sections/footer/footer.js
@@ -6,7 +6,7 @@ customElements.define('portfolio-footer', class extends HTMLElement {
           <p class="footer-copy">Copyright <span id="year"></span> Mara Vignesh. All rights reserved.</p>
           <div class="footer-links">
             <a href="https://github.com/mvh2005" target="_blank" rel="noopener noreferrer">GitHub</a>
-            <a href="https://www.linkedin.com/in/vignesh-mara-561942279/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/mara-vignesh-561942279/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           </div>
         </div>
       </footer>
